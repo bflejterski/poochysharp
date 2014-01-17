a216407d34d8d4dc11ab214f23627c4d251c0787
Bartek Flejterski <b.flejterski@gmail.com>
2014-01-17 15:45:15 +0100

    Utility class for reading app.config keys
    Convienient error checking and type convertion boundled in static Config
    class

---
+++ Utils.Test/ConfigTests.cs
@@ -0,0 +1,106 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConfigTests.cs" company="Bartek Flejterski">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2014 Bartek Flejterski
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+
+namespace Utils.Test
+{
+    /// <summary>
+    ///     The config tests.
+    /// </summary>
+    [TestClass]
+    public class ConfigTests
+    {
+        /// <summary>
+        ///     The test method 1.
+        /// </summary>
+        [TestMethod]
+        public void ReadIntTest()
+        {
+            var result = Config.ReadKey<int>("int");
+            Check.That(result).IsEqualTo(7);
+        }
+
+        /// <summary>
+        ///     The read double test.
+        /// </summary>
+        [TestMethod]
+        public void ReadDoubleTest()
+        {
+            var result = Config.ReadKey<double>("double");
+            Check.That(result).IsEqualTo(2.5);
+        }
+
+        /// <summary>
+        ///     The read string test.
+        /// </summary>
+        [TestMethod]
+        public void ReadStringTest()
+        {
+            var result = Config.ReadKey<string>("string");
+            Check.That(result).IsEqualTo("test");
+        }
+
+        /// <summary>
+        ///     The read all keys test.
+        /// </summary>
+        [TestMethod]
+        public void ReadAllKeysTest()
+        {
+            IEnumerable<string> allKeys = Config.AllKeys;
+            Check.That(allKeys).ContainsExactly(new[] {"int", "double", "string"});
+        }
+
+        /// <summary>
+        /// The read not existing key test.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof (UtilsException))]
+        public void ReadNotExistingKeyTest()
+        {
+            Config.ReadKey<string>("notExistingKey");
+        }
+
+        /// <summary>
+        /// The read key with non existing converter type test.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof (UtilsException))]
+        public void ReadKeyWithNonExistingConverterTypeTest()
+        {
+            Config.ReadKey<NonExistingConverterType>("string");
+        }
+    }
+
+    /// <summary>
+    /// The non existing converter type class - used interally for testing.
+    /// </summary>
+    internal class NonExistingConverterType
+    {
+    }
+}
---
+++ Utils/Config.cs
@@ -0,0 +1,98 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Config.cs" company="Bartek Flejterski">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2014 Bartek Flejterski
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+
+namespace Utils
+{
+    /// <summary>
+    ///     Utility class for reading keys from app.config using generics.
+    /// </summary>
+    public static class Config
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Gets the all keys from
+        /// </summary>
+        public static IEnumerable<string> AllKeys
+        {
+            get { return ConfigurationManager.AppSettings.AllKeys; }
+        }
+
+        /// <summary>
+        /// Gets key from app.config converted to the given type
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// </exception>
+        /// <exception cref="AppSettingsKeyNotFound">
+        /// </exception>
+        /// <exception cref="TypeConverterNotFound">
+        /// </exception>
+        public static T ReadKey<T>(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            if (!ConfigurationManager.AppSettings.AllKeys.Contains(key))
+            {
+                throw new UtilsException(string.Format("Key [{0}] has not been found in the configuration file.", key));
+            }
+
+            try
+            {
+                object convertFrom =
+                    TypeDescriptor.GetConverter(typeof (T)).ConvertFrom(ConfigurationManager.AppSettings[key]);
+                if (convertFrom != null)
+                {
+                    return (T) convertFrom;
+                }
+            }
+            catch (NotSupportedException e)
+            {
+                throw new UtilsException(string.Format("There's no default converter for [{0}].", typeof(T)), e);
+            }
+
+            throw new UtilsException(string.Format("There's no default converter for [{0}].", typeof (T)));
+        }
+
+        #endregion
+    }
+}
